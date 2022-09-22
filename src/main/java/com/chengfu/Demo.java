36f6c0e592e825d6c0e5b38ad7dd1297d2bde231
zx <1326508505@qq.com>
2022-09-22 20:15:24 +0800

    默认变更列表

---
+++ src/main/java/com/chengfu/Demo.java
@@ -0,0 +1,11 @@
+package com.chengfu;
+
+/**
+ * @author Meettry
+ * @date 2022/9/22 20:14
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
