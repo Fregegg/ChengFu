5254f14b106c8b64aabb476f684abbeb7408a3a8
Freg <Freg@personal.com>
2022-09-22 19:52:27 +0800

    Initial commit

---
+++ src/main/java/com/chengfu/ChengFuApplication.java
@@ -0,0 +1,13 @@
+package com.chengfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChengFuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChengFuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chengfu/ChengFuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChengFuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
